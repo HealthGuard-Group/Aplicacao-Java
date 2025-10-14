7fc31d6a16448e969e86f2bff15b17e0fd2e710d
Rfratini <rafael.corso@sptech.school>
2025-10-14 18:01:53 -0300

    Manutenção (Main) - Reparei o Main para capturar se está conectado ou desconectado da internet e criei a variavel 'conexao' que será usada para fazer os inserts no banco

--- healthguard_api_looca/src/main/java/guard/health/Main.java
+++ healthguard_api_looca/src/main/java/guard/health/Main.java
@@ -7,12 +7,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Looca looca = new Looca();
         Scanner leitorInicial = new Scanner(System.in);
-        List<RedeInterface> interfaces = looca.getRede().getGrupoDeInterfaces().getInterfaces();
-        Boolean redeConectada = false;
 
+        List<RedeInterface> interfaces = looca.getRede().getGrupoDeInterfaces().getInterfaces();
+        Boolean conexao;
         System.out.println("""
                                      _ _   _       ___                     _\s
                      /\\  /\\___  __ _| | |_| |__   / _ \\_   _  __ _ _ __ __| |
@@ -20,31 +20,79 @@ public class Main {
                    / __  /  __/ (_| | | |_| | | / /_\\\\ | |_| | (_| | |  | (_||
                    \\/ /_/ \\___|\\__,_|_|\\__|_| |_\\____/ \\__,_|\\__,_|_|  \\__,_|
                 
-                
+                    Verificação de Rede...
                 __________________________________________________________________
                 """);
 
-        for (int i = 0; i < interfaces.size(); i++) {
-            RedeInterface rede = interfaces.get(i);
-            List<String> ipv4 = rede.getEnderecoIpv4();
+        // Variáveis para guardar o estado da captura anterior.
+        long enviadosAnteriores = -1;
+        long recebidosAnteriores = -1;
+        String ipMonitorado = "";
+        Boolean primeiraExecucao = true;
+        while(true) {
+            boolean redeConectada = false;
 
-            for (int i1 = 0; i1 < ipv4.size(); i1++) {
-                String ip = ipv4.get(i1);
-                if (ip.startsWith("192.168")) {
-                    long enviados = rede.getBytesEnviados();
-                    long recebidos = rede.getBytesRecebidos();
+            searchLoop:
+            for (int i = 0; i < interfaces.size(); i++) {
+                RedeInterface rede = interfaces.get(i);
+                List<String> ipv4 = rede.getEnderecoIpv4();
 
-                    if (enviados > 0 || recebidos > 0) {
-                        System.out.println(" A rede (" + ip + ") está conectada.");
+                for (int i1 = 0; i1 < ipv4.size(); i1++) {
+                    String ip = ipv4.get(i1);
+
+                    if (ip.startsWith("172.") || ip.startsWith("192.168.") || ip.startsWith("10.") ) {
                         redeConectada = true;
-                    } else {
-                        System.out.println("A rede local (" + ip + ") não está conectada.");
+                        long enviados = rede.getBytesEnviados();
+                        long recebidos = rede.getBytesRecebidos();
+
+                        if ((enviadosAnteriores == -1 || !ip.equals(ipMonitorado)) && primeiraExecucao) {
+
+                            System.out.println("Rede (" + ip + ") Encontrada. Iniciando monitoramento.");
+                            primeiraExecucao = false;
+                            enviadosAnteriores = enviados;
+                            recebidosAnteriores = recebidos;
+                            ipMonitorado = ip;
+
+                        }  else if(enviadosAnteriores == -1 || !ip.equals(ipMonitorado)) {
+                            System.out.println("Rede (" + ip + ")desconectada - Tentando reconectar-se... ");
+                            enviadosAnteriores = enviados;
+                            recebidosAnteriores = recebidos;
+                            ipMonitorado = ip;
+                        } else {
+                            if (enviados > enviadosAnteriores || recebidos > recebidosAnteriores) {
+                                System.out.println("Rede (" + ip + ") está conectada. Houve tráfego.");
+                                conexao = true;
+                                enviadosAnteriores = enviados;
+                                recebidosAnteriores = recebidos;
+                            } else {
+                                System.out.println("ALERTA: Rede (" + ip + ") está Desconectada.");
+                                conexao = false;
+                                 enviadosAnteriores = -1;
+                                 recebidosAnteriores = -1;
+                                 ipMonitorado = "";
+                            }
+                        }
+
+
+                        break searchLoop; // esse searchLoop no break faz os dois for pararem
                     }
                 }
             }
-        }
-        if (!redeConectada) {
-            System.out.println(" Nenhuma interface de Rede Local ativa foi encontrada.");
+
+
+            if (!redeConectada) {
+
+                if (enviadosAnteriores != -1) {
+                    System.out.println("Nenhuma interface de Rede Local ativa foi encontrada. Conexão perdida.");
+                    enviadosAnteriores = -1;
+                    recebidosAnteriores = -1;
+                    ipMonitorado = "";
+                }
+            }
+
+            Thread.sleep(5000);
+            // inserir no banco dentro desta chave
+            // para a cada captura, mandar ao banco a variavel conexao(boleana) e ter no banco cada status de conexao
         }
     }
 }
